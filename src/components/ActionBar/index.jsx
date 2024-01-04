93ecd25d601d5e42c4d3db99b43639a779ee7993
upendra-educase <129498976+upendra-educase@users.noreply.github.com>
2024-01-04 15:18:54 +0530

    curriculum

--- src/components/ActionBar/index.jsx
+++ src/components/ActionBar/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Card, CardBody } from "react-bootstrap";
 import { Menu, LogOut } from "react-feather";
 
-function ActionBar() {
+function ActionBar({isAsideOpen, setAsideOpen}) {
 
   return (
     <>
       <Card className={`mb-4 d-xl-none`}>
         <CardBody>
           <div className="d-flex justify-content-between p-2">
-            <Menu color="#4D8EFF" />
+            <Menu color="#4D8EFF" onClick={() => setAsideOpen(!isAsideOpen)}/>
             <LogOut color="#4D8EFF" />
           </div>
         </CardBody>
