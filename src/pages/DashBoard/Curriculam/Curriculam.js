284ba9992574f57a6f9de230642aa1d154ee9605
upendra-educase <129498976+upendra-educase@users.noreply.github.com>
2024-01-05 13:59:27 +0530

    curriculam done

--- src/pages/DashBoard/Curriculam/Curriculam.js
+++ src/pages/DashBoard/Curriculam/Curriculam.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card } from 'react-bootstrap'
 import leftArrow from "../../../images/leftArrow.svg"
 import ButtonComponent from '../../../components/button/ButtonComponent'
 import "./curriculam.css"
+import NewCurriculam from './NewCurriculam/NewCurriculam'
 
 const Curriculam = () => {
+
+   const[curriculamState , setCurriculamState] = useState('Curriculam')
+
   return (
     <div>
-      <Card  >
+      {
+        curriculamState === 'Curriculam' ? <Card  >
         <div className='d-flex justify-content-between align-items-center p-4 flex-wrap'>
 
           <div className='d-flex justify-content-between align-items-center gap' >
@@ -18,11 +23,13 @@ const Curriculam = () => {
           </div>
           <div className='d-flex justify-content-center align-items-center gap'>
             <ButtonComponent buttonType={'secondary'} title={'Take A Tour'} />
-            <ButtonComponent buttonType={'primary'} title={'Add New'} />
+            <ButtonComponent buttonType={'primary'} title={'Add New'}  onClick={e => setCurriculamState('NewCurriculam')}/>
           </div>
 
         </div>
-      </Card>
+      </Card> :
+      <NewCurriculam />
+      }
     </div>
   )
 }
--- src/pages/DashBoard/DashBoard.js
+++ src/pages/DashBoard/DashBoard.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react'
 import Sidebar from '../../components/common/Sidebar'
 import Router from '../../router/Router'
 import ActionBar from '../../components/ActionBar'
+import "./DashBoard.css"
 
 const DashBoard = () => {
   const [isAsideOpen, setAsideOpen] = useState(false);
 
   return (
-    <div className='d-flex'>
-      <div>
-        <Sidebar isAsideOpen={isAsideOpen} setAsideOpen={setAsideOpen} />
-      </div>
+    <div className='d-flex dashboard-section'>
+
+      <Sidebar isAsideOpen={isAsideOpen} setAsideOpen={setAsideOpen} />
+
       <div className='py-4 w-100'>
         <ActionBar isAsideOpen={isAsideOpen} setAsideOpen={setAsideOpen} />
         <Router />
