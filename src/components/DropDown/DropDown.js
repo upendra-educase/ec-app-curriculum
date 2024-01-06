b1049d2101ac2b54a885ae7da7f94bcd5eeb1eb7
upendra-educase <129498976+upendra-educase@users.noreply.github.com>
2024-01-06 14:13:52 +0530

    curriculam ongoing

---
+++ src/components/DropDown/DropDown.js
@@ -0,0 +1,78 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// const Dropdown = ({value, data, placeholder, styleClass, onChange}) => {
+
+//     const handleChange = (event) => {
+//         const {value} = event.target;
+//         onChange(value);
+//     };
+
+//     return (
+//         <div className={`form-group ${styleClass}`}>
+//             <select
+//                 value={value}
+//                 className="form-control"
+//                 onChange={handleChange}>
+//                 <option value="">{placeholder}</option>
+//                 {data.map((item, key) => (
+//                     <option
+//                         key={key}
+//                         value={item.value}>
+//                         {item.label}
+//                     </option>
+//                 ))}
+//             </select>
+//         </div>
+//     )
+// };
+
+// Dropdown.propTypes = {
+//     value: PropTypes.string,
+//     placeholder: PropTypes.string,
+//     data: PropTypes.array.isRequired,
+//     styleClass: PropTypes.string,
+//     onChange: PropTypes.func.isRequired
+// };
+
+// Dropdown.defaultProps = {
+//     value: '',
+//     styleClass: '',
+//     placeholder: ''
+// };
+
+// export default Dropdown;
+
+import React, { useState } from 'react';
+import Select from 'react-select';
+import "./DropDown.css"
+
+
+
+function DropDown({options,placeholder,defaultValue,onChange,isClearable,isSearchable,id}) {
+  
+
+  return (
+    <div className="App">
+      <Select
+        defaultValue={defaultValue}
+        onChange={onChange}
+        options={options}
+        placeholder={placeholder}
+        isClearable={isClearable}
+        isSearchable={isSearchable}
+        id={id}
+      />
+    </div>
+  );
+}
+DropDown.defaultProps = {
+  isClearable: false,
+  require: false,
+  placeholder: "Select...",
+  isSearchable: true,
+  isDisabled: false,
+  isLoading: false
+}
+
+export default DropDown;
