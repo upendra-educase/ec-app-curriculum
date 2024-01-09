9dc334b6d96f9e70210e089aa3465e7d3ea5cfc3
upendra-educase <129498976+upendra-educase@users.noreply.github.com>
2024-01-09 18:46:56 +0530

    Curriculum

--- src/components/common/CardWithImage/index.js
+++ src/components/common/CardWithImage/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Card, CardBody, CardText } from 'react-bootstrap'
 import cross from "../../../images/+.svg"
+import { PlayCircle } from 'react-feather'
+import { FaRegFilePdf } from "react-icons/fa6";
+import "./index.css"
 
-const CardWithImage = () => {
+const CardWithImage = ({ link, title, type }) => {
     return (
         <Card
             style={{
                 width: '350px',
                 height: '240px',
-                borderRadius: "20px",
+                borderRadius: "8px",
                 // boxShadow: "0px 0px 5px 0px #001B4A33",
                 // padding:'1rem'
                 border: "1px solid #C4C4C4",
-                backgroundColor: 'aliceblue  ',
                 overflow: 'hidden'
 
             }}
@@ -20,19 +22,19 @@ const CardWithImage = () => {
             <div style={{ height: '70%', position: 'relative' }}>
                 <img
                     alt="Sample"
-                    src="https://picsum.photos/300/200"
+                    src={link ? link : "https://picsum.photos/300/200"}
                     style={{ height: '100%', width: '100%', borderTopLeftRadius: '20px', borderTopRightRadius: '20px' }}
                 />
-                <div style={{ position: "absolute", top: '15px', right: '15px', height: "29px", width: '29px', borderRadius: "100%", backgroundColor: "#FFFFFF",cursor:'pointer' }} className='d-flex justify-content-center align-items-center'>
+                <div style={{ position: "absolute", top: '15px', right: '15px', height: "29px", width: '29px', borderRadius: "100%", backgroundColor: "#FFFFFF", cursor: 'pointer' }} className='d-flex justify-content-center align-items-center'>
                     <img src={cross} />
                 </div>
             </div>
 
             <CardBody className='d-flex align-items-center'>
-                <CardText className='d-flex align-items-center  px-2'>
-                    History.pdf
-                </CardText>
-
+                <div className='data-type-icon'>
+                    {type === "pdf" ? <PlayCircle /> : <FaRegFilePdf />}
+                </div>
+                <div className='px-2'>{title}</div>
             </CardBody>
         </Card>
     )
